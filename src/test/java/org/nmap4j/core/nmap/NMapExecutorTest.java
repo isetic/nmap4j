48f42cf67de7a666771a4268e70b356271fa4b25
José Miguel Martínez <josemiguel.martinez@isetic.com>
2018-02-13 11:13:01 +0100

    unformatting

--- src/test/java/org/nmap4j/core/nmap/NMapExecutorTest.java
+++ src/test/java/org/nmap4j/core/nmap/NMapExecutorTest.java
@@ -11,95 +11,95 @@ import static org.junit.Assert.fail;
 
 public class NMapExecutorTest {
 
-    private ArgumentProperties nmapArgs;
-    private NMapProperties nmapProps;
+	private ArgumentProperties nmapArgs;
+	private NMapProperties nmapProps;
 
-    @Before
-    public void setup() {
-        nmapArgs = new ArgumentProperties();
-        nmapProps = new NMapProperties();
-    }
+	@Before
+	public void setup() {
+		nmapArgs = new ArgumentProperties();
+		nmapProps = new NMapProperties();
+	}
 
-    @After
-    public void teardown() {
-        nmapArgs = null;
-        nmapProps = null;
-    }
+	@After
+	public void teardown() {
+		nmapArgs = null;
+		nmapProps = null;
+	}
 
-    /**
-     * This tests whether or not we fail with an exception when we try
-     * to instantiate the object with nulls which isn't a valide use case.
-     */
-    @Test
-    public void testConstructionWithNulls() {
-        boolean hasFailed = false;
-        // expect this to throw an exception
-        try {
-            NMapExecutor nmapExec = new NMapExecutor(null, null);
-        } catch (NMapInitializationException e) {
-            // this is what I would expect to happen
-            hasFailed = true;
-        }
+	/**
+	 * This tests whether or not we fail with an exception when we try
+	 * to instantiate the object with nulls which isn't a valide use case.
+	 */
+	@Test
+	public void testConstructionWithNulls() {
+		boolean hasFailed = false;
+		// expect this to throw an exception
+		try {
+			NMapExecutor nmapExec = new NMapExecutor(null, null);
+		} catch (NMapInitializationException e) {
+			// this is what I would expect to happen
+			hasFailed = true;
+		}
 
-        if (!hasFailed) {
-            fail();
-        }
-    }
+		if (!hasFailed) {
+			fail();
+		}
+	}
 
-    /**
-     * This test verifies that the initialization fails due to there being
-     * no path to nmap.
-     */
-    @Test
-    public void testConstructionWithNoPath() {
-        boolean hasFailed = false;
-        // expect this to throw an exception
-        try {
-            NMapExecutor nmapExec = new NMapExecutor(nmapArgs, nmapProps);
-        } catch (NMapInitializationException e) {
-            // this is what I would expect to happen
-            hasFailed = true;
-        }
+	/**
+	 * This test verifies that the initialization fails due to there being
+	 * no path to nmap.
+	 */
+	@Test
+	public void testConstructionWithNoPath() {
+		boolean hasFailed = false;
+		// expect this to throw an exception
+		try {
+			NMapExecutor nmapExec = new NMapExecutor(nmapArgs, nmapProps);
+		} catch (NMapInitializationException e) {
+			// this is what I would expect to happen
+			hasFailed = true;
+		}
 
-        if (!hasFailed) {
-            fail();
-        }
-    }
+		if (!hasFailed) {
+			fail();
+		}
+	}
 
-    @Test
-    public void testZeroLengthNMapPath() {
-        nmapProps.setPath("");
+	@Test
+	public void testZeroLengthNMapPath() {
+		nmapProps.setPath("");
 
-        boolean hasFailed = false;
-        // expect this to throw an exception
-        try {
-            NMapExecutor nmapExec = new NMapExecutor(nmapArgs, nmapProps);
-        } catch (NMapInitializationException e) {
-            // this is what I would expect to happen
-            hasFailed = true;
-        }
+		boolean hasFailed = false;
+		// expect this to throw an exception
+		try {
+			NMapExecutor nmapExec = new NMapExecutor(nmapArgs, nmapProps);
+		} catch (NMapInitializationException e) {
+			// this is what I would expect to happen
+			hasFailed = true;
+		}
 
-        if (!hasFailed) {
-            fail();
-        }
-    }
+		if (!hasFailed) {
+			fail();
+		}
+	}
 
-    @Test
-    public void testExecuteMethodWithoutSudo() throws NMapInitializationException, NMapExecutionException {
+	@Test
+	public void testExecuteMethodWithoutSudo() throws NMapInitializationException, NMapExecutionException {
 
-        nmapProps.setPath(NmapTestUtils.findNmapPath());
+		nmapProps.setPath(NmapTestUtils.findNmapPath());
 
-        nmapArgs.addFlag(Flag.AGGRESIVE_TIMING);
-        nmapArgs.addFlag(Flag.VERSION);
-        nmapArgs.addFlag(Flag.OS_DETECTION);
+		nmapArgs.addFlag(Flag.AGGRESIVE_TIMING);
+		nmapArgs.addFlag(Flag.VERSION);
+		nmapArgs.addFlag(Flag.OS_DETECTION);
 
-        nmapArgs.addIncludedHost("localhost");
+		nmapArgs.addIncludedHost("localhost");
 
 
-        NMapExecutor nmapExec = new NMapExecutor(nmapArgs, nmapProps);
-        ExecutionResults results = nmapExec.execute();
-        System.out.println(results.getOutput());
+		NMapExecutor nmapExec = new NMapExecutor(nmapArgs, nmapProps);
+		ExecutionResults results = nmapExec.execute();
+		System.out.println(results.getOutput());
 
-    }
+	}
 
 }
