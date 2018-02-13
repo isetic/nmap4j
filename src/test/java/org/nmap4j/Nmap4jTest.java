ccac453bab3a487543f4054a643cf5897731da0f
José Miguel Martínez <josemiguel.martinez@isetic.com>
2018-02-13 12:23:00 +0100

    unformatting

--- src/test/java/org/nmap4j/Nmap4jTest.java
+++ src/test/java/org/nmap4j/Nmap4jTest.java
@@ -9,27 +9,27 @@ import static org.junit.Assert.fail;
 
 public class Nmap4jTest {
 
-    @Test
-    public void basicNmap4jUsageTest() throws NMapExecutionException, NMapInitializationException {
+	@Test
+	public void basicNmap4jUsageTest() throws NMapExecutionException, NMapInitializationException {
 
 
-        Nmap4j nmap4j = new Nmap4j(NmapTestUtils.findNmapPath());
-        nmap4j.addFlags("-sV -T5 -oX -");
-        nmap4j.includeHosts("localhost");
-        nmap4j.execute();
-        if (!nmap4j.hasError()) {
-            NMapRun nmapRun = nmap4j.getResult();
-            String output = nmap4j.getOutput();
-            if (output == null) {
-                fail();
-            }
-            String errors = nmap4j.getExecutionResults().getErrors();
-            if (errors == null) {
-                fail();
-            }
-        }
+		Nmap4j nmap4j = new Nmap4j( NmapTestUtils.findNmapPath() );
+		nmap4j.addFlags( "-sV -T5 -oX -" );
+		nmap4j.includeHosts( "localhost" );
+		nmap4j.execute();
+		if (!nmap4j.hasError()) {
+			NMapRun nmapRun = nmap4j.getResult();
+			String output = nmap4j.getOutput();
+			if (output == null) {
+				fail();
+			}
+			String errors = nmap4j.getExecutionResults().getErrors();
+			if (errors == null) {
+				fail();
+			}
+		}
 
 
-    }
+	}
 
 }
