5dda144b64a6e7ec4fc93c391420ca3fe732cbdd
José Miguel Martínez <josemiguel.martinez@isetic.com>
2018-02-13 11:14:30 +0100

    unformatting

--- src/test/java/org/nmap4j/core/scans/BaseScanTest.java
+++ src/test/java/org/nmap4j/core/scans/BaseScanTest.java
@@ -10,30 +10,30 @@ import org.nmap4j.core.scans.IScan.TimingFlag;
 
 public class BaseScanTest {
 
-    @Test
-    public void testSimpleScan() throws NMapExecutionException, NMapInitializationException, ParameterValidationFailureException {
+	@Test
+	public void testSimpleScan() throws NMapExecutionException, NMapInitializationException, ParameterValidationFailureException {
 
-        BaseScan baseScan = new BaseScan(NmapTestUtils.findNmapPath());
+		BaseScan baseScan = new BaseScan(NmapTestUtils.findNmapPath());
 
-        baseScan.includeHost("localhost");
-        baseScan.addPorts(new int[]{22, 80, 443, 3306});
-        baseScan.addFlag(Flag.OS_DETECTION);
-        baseScan.setTiming(TimingFlag.AGGRESSIVE);
+		baseScan.includeHost("localhost");
+		baseScan.addPorts(new int[]{22, 80, 443, 3306});
+		baseScan.addFlag(Flag.OS_DETECTION);
+		baseScan.setTiming(TimingFlag.AGGRESSIVE);
 
-        System.out.println(baseScan.getArgumentProperties().getFlags());
-        System.out.println(baseScan.getNMapProperties().getFullyFormattedCommand());
-
-
-        ExecutionResults results = baseScan.executeScan();
-        System.out.println(results.getExecutedCommand());
-        System.out.println(results.getOutput());
-        if (results.hasErrors()) {
-            System.out.println("Errors: " + results.getErrors());
-        } else {
-            System.out.println("Results: " + results.getOutput());
-        }
+		System.out.println(baseScan.getArgumentProperties().getFlags());
+		System.out.println(baseScan.getNMapProperties().getFullyFormattedCommand());
 
 
-    }
+		ExecutionResults results = baseScan.executeScan();
+		System.out.println(results.getExecutedCommand());
+		System.out.println(results.getOutput());
+		if (results.hasErrors()) {
+			System.out.println("Errors: " + results.getErrors());
+		} else {
+			System.out.println("Results: " + results.getOutput());
+		}
+
+
+	}
 
 }
