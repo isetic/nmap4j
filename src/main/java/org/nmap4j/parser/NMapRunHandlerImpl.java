21dbb794f98f07b1e72f393e04afda7f5ba3b663
José Miguel Martínez <josemiguel.martinez@isetic.com>
2018-02-13 12:21:46 +0100

    unformatting

--- src/main/java/org/nmap4j/parser/NMapRunHandlerImpl.java
+++ src/main/java/org/nmap4j/parser/NMapRunHandlerImpl.java
@@ -60,25 +60,25 @@ import org.slf4j.LoggerFactory;
 import org.xml.sax.Attributes;
 
 public class NMapRunHandlerImpl implements INMapRunHandler {
-	private final org.slf4j.Logger logger = LoggerFactory.getLogger(Thread.currentThread().getStackTrace()[0].getClassName());
+	private final org.slf4j.Logger logger = LoggerFactory.getLogger( Thread.currentThread().getStackTrace()[0].getClassName() );
 
 	public Address createAddress(Attributes attributes) {
 		Address address = new Address();
-		address.setAddr(attributes.getValue(Address.ADDR_ATTR));
-		address.setAddrtype(attributes.getValue(Address.ADDRTYPE_ATTR));
-		address.setVendor(attributes.getValue(Address.VENDOR_ATTR));
+		address.setAddr( attributes.getValue( Address.ADDR_ATTR ) );
+		address.setAddrtype( attributes.getValue( Address.ADDRTYPE_ATTR ) );
+		address.setVendor( attributes.getValue( Address.VENDOR_ATTR ) );
 		return address;
 	}
 
 	public Debugging createDebugging(Attributes attributes) {
 		Debugging debugging = new Debugging();
-		debugging.setLevel(Long.parseLong(attributes.getValue(Debugging.LEVEL_TAG)));
+		debugging.setLevel( Long.parseLong( attributes.getValue( Debugging.LEVEL_TAG ) ) );
 		return debugging;
 	}
 
 	public Distance createDistance(Attributes attributes) {
 		Distance distance = new Distance();
-		distance.setValue(Long.parseLong(attributes.getValue(Distance.VALUE_ATTR)));
+		distance.setValue( Long.parseLong( attributes.getValue( Distance.VALUE_ATTR ) ) );
 		return distance;
 	}
 
@@ -94,11 +94,11 @@ public class NMapRunHandlerImpl implements INMapRunHandler {
 
 	public Host createHost(Attributes attributes) {
 		Host host = new Host();
-		if (attributes.getValue(Host.STARTTIME_ATTR) != null) {
-			host.setStartTime(Long.parseLong(attributes.getValue(Host.STARTTIME_ATTR)));
+		if (attributes.getValue( Host.STARTTIME_ATTR ) != null) {
+			host.setStartTime( Long.parseLong( attributes.getValue( Host.STARTTIME_ATTR ) ) );
 		}
-		if (attributes.getValue(Host.ENDTIME_ATTR) != null) {
-			host.setEndTime(Long.parseLong(attributes.getValue(Host.ENDTIME_ATTR)));
+		if (attributes.getValue( Host.ENDTIME_ATTR ) != null) {
+			host.setEndTime( Long.parseLong( attributes.getValue( Host.ENDTIME_ATTR ) ) );
 		}
 
 		return host;
@@ -110,9 +110,9 @@ public class NMapRunHandlerImpl implements INMapRunHandler {
 
 	public Hosts createHosts(Attributes attributes) {
 		Hosts hosts = new Hosts();
-		hosts.setDown(Long.parseLong(attributes.getValue(Hosts.DOWN_ATTR)));
-		hosts.setTotal(Long.parseLong(attributes.getValue(Hosts.TOTAL_ATTR)));
-		hosts.setUp(Long.parseLong(attributes.getValue(Hosts.UP_ATTR)));
+		hosts.setDown( Long.parseLong( attributes.getValue( Hosts.DOWN_ATTR ) ) );
+		hosts.setTotal( Long.parseLong( attributes.getValue( Hosts.TOTAL_ATTR ) ) );
+		hosts.setUp( Long.parseLong( attributes.getValue( Hosts.UP_ATTR ) ) );
 
 		return hosts;
 	}
@@ -126,12 +126,12 @@ public class NMapRunHandlerImpl implements INMapRunHandler {
 
 		NMapRun run = new NMapRun();
 
-		run.setArgs(attributes.getValue(NMapRun.ARGS_ATTR));
-		run.setScanner(attributes.getValue(NMapRun.SCANNER_ATTR));
-		run.setStart(attributes.getValue(NMapRun.START_ATTR));
-		run.setStartstr(attributes.getValue(NMapRun.STARTSTR_ATTR));
-		run.setVersion(attributes.getValue(NMapRun.VERSION_ATTR));
-		run.setXmloutputversion(attributes.getValue(NMapRun.XML_OUTPUT_VERSION_ATTR));
+		run.setArgs( attributes.getValue( NMapRun.ARGS_ATTR ) );
+		run.setScanner( attributes.getValue( NMapRun.SCANNER_ATTR ) );
+		run.setStart( attributes.getValue( NMapRun.START_ATTR ) );
+		run.setStartstr( attributes.getValue( NMapRun.STARTSTR_ATTR ) );
+		run.setVersion( attributes.getValue( NMapRun.VERSION_ATTR ) );
+		run.setXmloutputversion( attributes.getValue( NMapRun.XML_OUTPUT_VERSION_ATTR ) );
 
 		return run;
 	}
@@ -142,35 +142,35 @@ public class NMapRunHandlerImpl implements INMapRunHandler {
 
 	public OsClass createOsClass(Attributes attributes) {
 		OsClass osClass = new OsClass();
-		osClass.setAccuracy(attributes.getValue(OsClass.ACCURACY_ATTR));
-		osClass.setOsfamily(attributes.getValue(OsClass.OSFAMILY_ATTR));
-		osClass.setOsgen(attributes.getValue(OsClass.OSGEN_ATTR));
-		osClass.setType(attributes.getValue(OsClass.TYPE_ATTR));
-		osClass.setVendor(attributes.getValue(OsClass.VENDOR_ATTR));
+		osClass.setAccuracy( attributes.getValue( OsClass.ACCURACY_ATTR ) );
+		osClass.setOsfamily( attributes.getValue( OsClass.OSFAMILY_ATTR ) );
+		osClass.setOsgen( attributes.getValue( OsClass.OSGEN_ATTR ) );
+		osClass.setType( attributes.getValue( OsClass.TYPE_ATTR ) );
+		osClass.setVendor( attributes.getValue( OsClass.VENDOR_ATTR ) );
 
 		return osClass;
 	}
 
 	public OsMatch createOsMatch(Attributes attributes) {
 		OsMatch osMatch = new OsMatch();
-		osMatch.setAccuracy(attributes.getValue(OsMatch.ACCURACY_ATTR));
-		osMatch.setLine(attributes.getValue(OsMatch.LINE_ATTR));
-		osMatch.setName(attributes.getValue(OsMatch.NAME_ATTR));
+		osMatch.setAccuracy( attributes.getValue( OsMatch.ACCURACY_ATTR ) );
+		osMatch.setLine( attributes.getValue( OsMatch.LINE_ATTR ) );
+		osMatch.setName( attributes.getValue( OsMatch.NAME_ATTR ) );
 		return osMatch;
 	}
 
 	public Port createPort(Attributes attributes) {
 		Port port = new Port();
-		port.setPortId(Long.parseLong(attributes.getValue(Port.PORTID_ATTR)));
-		port.setProtocol(attributes.getValue(Port.PROTOCOL_ATTR));
+		port.setPortId( Long.parseLong( attributes.getValue( Port.PORTID_ATTR ) ) );
+		port.setProtocol( attributes.getValue( Port.PROTOCOL_ATTR ) );
 		return port;
 	}
 
 	public PortUsed createPortUsed(Attributes attributes) {
 		PortUsed portUsed = new PortUsed();
-		portUsed.setPortid(attributes.getValue(PortUsed.PORTID_ATTR));
-		portUsed.setProto(attributes.getValue(PortUsed.PROTO_ATTR));
-		portUsed.setState(attributes.getValue(PortUsed.STATE_ATTR));
+		portUsed.setPortid( attributes.getValue( PortUsed.PORTID_ATTR ) );
+		portUsed.setProto( attributes.getValue( PortUsed.PROTO_ATTR ) );
+		portUsed.setState( attributes.getValue( PortUsed.STATE_ATTR ) );
 		return portUsed;
 	}
 
@@ -185,48 +185,48 @@ public class NMapRunHandlerImpl implements INMapRunHandler {
 	public ScanInfo createScanInfo(Attributes attributes) {
 		ScanInfo scan = new ScanInfo();
 
-		scan.setNumservices(Long.parseLong(
-				attributes.getValue(ScanInfo.NUM_SERVICES_ATTR)));
-		scan.setProtocol(attributes.getValue(ScanInfo.PROTOCOL_ATTR));
-		scan.setServices(attributes.getValue(ScanInfo.SERVICES_ATTR));
-		scan.setType(attributes.getValue(ScanInfo.TYPE_ATTR));
+		scan.setNumservices( Long.parseLong(
+				attributes.getValue( ScanInfo.NUM_SERVICES_ATTR ) ) );
+		scan.setProtocol( attributes.getValue( ScanInfo.PROTOCOL_ATTR ) );
+		scan.setServices( attributes.getValue( ScanInfo.SERVICES_ATTR ) );
+		scan.setType( attributes.getValue( ScanInfo.TYPE_ATTR ) );
 
 		return scan;
 	}
 
 	public Service createService(Attributes attributes) {
 		Service service = new Service();
-		service.setConf(attributes.getValue(Service.CONF_ATTR));
-		service.setExtrainfo(attributes.getValue(Service.EXTRA_INFO_ATTR));
-		service.setMethod(attributes.getValue(Service.METHOD_ATTR));
-		service.setName(attributes.getValue(Service.NAME_ATTR));
-		service.setOsType(attributes.getValue(Service.OS_TYPE_ATTR));
-		service.setProduct(attributes.getValue(Service.PRODUCT_ATTR));
-		service.setVersion(attributes.getValue(Service.VERSION_ATTR));
+		service.setConf( attributes.getValue( Service.CONF_ATTR ) );
+		service.setExtrainfo( attributes.getValue( Service.EXTRA_INFO_ATTR ) );
+		service.setMethod( attributes.getValue( Service.METHOD_ATTR ) );
+		service.setName( attributes.getValue( Service.NAME_ATTR ) );
+		service.setOsType( attributes.getValue( Service.OS_TYPE_ATTR ) );
+		service.setProduct( attributes.getValue( Service.PRODUCT_ATTR ) );
+		service.setVersion( attributes.getValue( Service.VERSION_ATTR ) );
 		return service;
 	}
 
 	public State createState(Attributes attributes) {
 		State state = new State();
-		state.setState(attributes.getValue(State.STATE_ATTR));
-		state.setReason(attributes.getValue(State.REASON_ATTR));
-		state.setReason_ttl(Long.parseLong(attributes.getValue(State.REASON_TTL_ATTR)));
+		state.setState( attributes.getValue( State.STATE_ATTR ) );
+		state.setReason( attributes.getValue( State.REASON_ATTR ) );
+		state.setReason_ttl( Long.parseLong( attributes.getValue( State.REASON_TTL_ATTR ) ) );
 
 		return state;
 	}
 
 	public Status createStatus(Attributes attributes) {
 		Status status = new Status();
-		status.setReason(attributes.getValue(Status.REASON_ATTR));
-		status.setState(attributes.getValue(Status.STATE_ATTR));
+		status.setReason( attributes.getValue( Status.REASON_ATTR ) );
+		status.setState( attributes.getValue( Status.STATE_ATTR ) );
 		return status;
 	}
 
 	public TcpSequence createTcpSequence(Attributes attributes) {
 		TcpSequence tcpSequence = new TcpSequence();
-		tcpSequence.setDifficulty(attributes.getValue(TcpSequence.DIFFICULTY_ATTR));
-		tcpSequence.setIndex(Long.parseLong(attributes.getValue(TcpSequence.INDEX_ATTR)));
-		tcpSequence.setValues(attributes.getValue(TcpSequence.VALUES_ATTR));
+		tcpSequence.setDifficulty( attributes.getValue( TcpSequence.DIFFICULTY_ATTR ) );
+		tcpSequence.setIndex( Long.parseLong( attributes.getValue( TcpSequence.INDEX_ATTR ) ) );
+		tcpSequence.setValues( attributes.getValue( TcpSequence.VALUES_ATTR ) );
 
 
 		return tcpSequence;
@@ -234,25 +234,25 @@ public class NMapRunHandlerImpl implements INMapRunHandler {
 
 	public TcpTsSequence createTcpTsSequence(Attributes attributes) {
 		TcpTsSequence tcpTsSequence = new TcpTsSequence();
-		tcpTsSequence.setaClass(attributes.getValue(TcpTsSequence.CLASS_ATTR));
-		tcpTsSequence.setValues(attributes.getValue(TcpTsSequence.VALUES_ATTR));
+		tcpTsSequence.setaClass( attributes.getValue( TcpTsSequence.CLASS_ATTR ) );
+		tcpTsSequence.setValues( attributes.getValue( TcpTsSequence.VALUES_ATTR ) );
 		return tcpTsSequence;
 	}
 
 	public Times createTimes(Attributes attributes) {
 		Times times = new Times();
-		times.setRttvar(Long.parseLong(attributes.getValue(Times.RTTVAR_ATTR)));
-		times.setSrtt(Long.parseLong(attributes.getValue(Times.SRTT_ATTR)));
-		times.setTo(Long.parseLong(attributes.getValue(Times.TO_ATTR)));
+		times.setRttvar( Long.parseLong( attributes.getValue( Times.RTTVAR_ATTR ) ) );
+		times.setSrtt( Long.parseLong( attributes.getValue( Times.SRTT_ATTR ) ) );
+		times.setTo( Long.parseLong( attributes.getValue( Times.TO_ATTR ) ) );
 
 		return times;
 	}
 
 	public Uptime createUptime(Attributes attributes) {
 		Uptime uptime = new Uptime();
-		uptime.setLastboot(attributes.getValue(Uptime.LASTBOOT_ATTR));
-		if (attributes.getValue(Uptime.UPTIME_TAG) != null) {
-			uptime.setSeconds(Long.parseLong(attributes.getValue(Uptime.UPTIME_TAG)));
+		uptime.setLastboot( attributes.getValue( Uptime.LASTBOOT_ATTR ) );
+		if (attributes.getValue( Uptime.UPTIME_TAG ) != null) {
+			uptime.setSeconds( Long.parseLong( attributes.getValue( Uptime.UPTIME_TAG ) ) );
 		}
 
 		return uptime;
@@ -260,24 +260,24 @@ public class NMapRunHandlerImpl implements INMapRunHandler {
 
 	public Verbose createVerbose(Attributes attributes) {
 		Verbose verbose = new Verbose();
-		verbose.setLevel(Long.parseLong(attributes.getValue(Verbose.LEVEL_ATTR)));
+		verbose.setLevel( Long.parseLong( attributes.getValue( Verbose.LEVEL_ATTR ) ) );
 
 		return verbose;
 	}
 
 	public Finished createFinished(Attributes attributes) {
 		Finished finished = new Finished();
-		finished.setElapsed(attributes.getValue(Finished.ELAPSED_ATTR));
-		finished.setTime(attributes.getValue(Finished.TIME_ATTR));
-		finished.setTimestr(attributes.getValue(Finished.TIMESTR_ATTR));
+		finished.setElapsed( attributes.getValue( Finished.ELAPSED_ATTR ) );
+		finished.setTime( attributes.getValue( Finished.TIME_ATTR ) );
+		finished.setTimestr( attributes.getValue( Finished.TIMESTR_ATTR ) );
 
 		return finished;
 	}
 
 	public Hostname createHostname(Attributes attributes) {
 		Hostname hostname = new Hostname();
-		hostname.setName(attributes.getValue(Hostname.NAME_ATTR));
-		hostname.setType(attributes.getValue(Hostname.TYPE_ATTR));
+		hostname.setName( attributes.getValue( Hostname.NAME_ATTR ) );
+		hostname.setType( attributes.getValue( Hostname.TYPE_ATTR ) );
 		return hostname;
 	}
 
@@ -293,26 +293,26 @@ public class NMapRunHandlerImpl implements INMapRunHandler {
 	public Trace createTrace(Attributes attributes) {
 		Trace trace = new Trace();
 
-		String portValue = attributes.getValue(Trace.PORT_ATTR);
+		String portValue = attributes.getValue( Trace.PORT_ATTR );
 		if (portValue != null)
-			trace.setPort(Long.parseLong(portValue));
-		String protValue = attributes.getValue(Trace.PORTOCOL_ATTR);
+			trace.setPort( Long.parseLong( portValue ) );
+		String protValue = attributes.getValue( Trace.PORTOCOL_ATTR );
 		if (protValue != null)
-			trace.setProtocol(protValue);
+			trace.setProtocol( protValue );
 		return trace;
 	}
 
 	public Hop createHop(Attributes attributes) {
 		Hop hop = new Hop();
 
-		hop.setTtl(Integer.parseInt(attributes.getValue(Hop.TTL_ATTR)));
-		hop.setIpaddr(attributes.getValue(Hop.IPADDR_ATTR));
-		String rtt = attributes.getValue(Hop.RTT_ATTR);
+		hop.setTtl( Integer.parseInt( attributes.getValue( Hop.TTL_ATTR ) ) );
+		hop.setIpaddr( attributes.getValue( Hop.IPADDR_ATTR ) );
+		String rtt = attributes.getValue( Hop.RTT_ATTR );
 		// sometimes we find "--" value
-		if ("--".equals(rtt)) {
-			logger.info("Bad rtt for hop --");
+		if ("--".equals( rtt )) {
+			logger.info( "Bad rtt for hop --" );
 		} else {
-			hop.setRtt(Float.parseFloat(rtt));
+			hop.setRtt( Float.parseFloat( rtt ) );
 		}
 
 		return hop;
@@ -328,15 +328,15 @@ public class NMapRunHandlerImpl implements INMapRunHandler {
 		Script script = new Script();
 		int ll = attributes.getLength();
 		for (int cont = 0; cont < ll; cont++) {
-			String value = attributes.getValue(cont);
-			String name = attributes.getQName(cont);
-			String type = attributes.getType(cont);
-			String localName = attributes.getLocalName(cont);
-			String uri = attributes.getURI(cont);
-			if (name.equals("id")) {
-				script.setId(value);
-			} else if (name.equals("output")) {
-				script.setOutput(value);
+			String value = attributes.getValue( cont );
+			String name = attributes.getQName( cont );
+			String type = attributes.getType( cont );
+			String localName = attributes.getLocalName( cont );
+			String uri = attributes.getURI( cont );
+			if (name.equals( "id" )) {
+				script.setId( value );
+			} else if (name.equals( "output" )) {
+				script.setOutput( value );
 			} else {
 				throw new RuntimeException();
 			}
